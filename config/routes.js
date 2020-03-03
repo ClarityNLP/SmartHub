71e49f72a90637a39e850e3a1173417552c89dba
Trey Schneider <trey@codeschneider.com>
2020-03-03 14:40:09 -0500

    update routes

--- config/routes.js
+++ config/routes.js
@@ -1,31 +1,17 @@
-/**
- * Route Mappings
- * (sails.config.routes)
- *
- * Your routes tell Sails what to do each time it receives a request.
- *
- * For more information on configuring custom routes, check out:
- * https://sailsjs.com/anatomy/config/routes-js
- */
-
 module.exports.routes = {
+  /* Autofill */
+  'get /autofill/:autofillId': 'AutofillController.getAutofill',
+  'post /activities/:activityId/questions/:questionSlug/autofill': 'AutofillController.doAutofill',
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
-
-  'post /activities/:activityId/questions/:questionSlug/autofill': 'QuestionController.autofill',
-
+  /* Form */
   'post /forms': 'FormController.create',
   'get /forms': 'FormController.all',
+  'post /schemas/v2/validate': 'FormController.validate',
 
+  /* Evidence */
   'post /activities/:activityId/evidences/:evidenceSlug': 'EvidenceController.create',
 
+  /* Activity */
   // 'get /subjects/:subjectId/forms/:formSlug': 'ActivityController.findOrCreate',
   'post /activities': 'ActivityController.create',
   'get /activities/:activityId': 'ActivityController.findById',
@@ -34,27 +20,12 @@ module.exports.routes = {
   'put /activities/:activityId/values': 'ActivityController.updateValues',
   'put /activities/:activityId/groups': 'ActivityController.updateGroupLoadingStates',
 
+  /* Job */
   'post /jobs': 'JobController.create',
   'get /jobs': 'JobController.all',
   'get /jobs/:jobId': 'JobController.findById', //top-level job metadata
 
-  'post /schemas/v2/validate': 'FormController.validate',
-
   'get /web-socket': 'JobController.testWebSocket'
 
   // 'get /jobs/:jobId/activities' //populate activites in the job (show statuses as well)
-
-
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
-
-
 };

269b567b626d32db66b878df1ed736a1cb077906
Trey Schneider <trey@codeschneider.com>
2020-03-03 14:37:30 -0500

    import form schema in validate form service

--- api/helpers/validate-form.js
+++ api/helpers/validate-form.js
@@ -1,6 +1,4 @@
-const Joi = require('joi');
-const map = require("async/map");
-const ObjectID = require('mongodb').ObjectID;
+const formSchema = require('../../schemas/form');
 
 module.exports = {
 
@@ -18,95 +16,7 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     try {
-      const schema = Joi.object({
-        name: Joi.string().required(),
-        slug: Joi.string().required(),
-        groups: Joi.object({
-          byId: Joi.object().pattern(
-            /^/,
-            Joi.object({
-              name: Joi.string().required(),
-              questions: Joi.object({
-                byId: Joi.object().pattern(
-                  /^/,
-                  Joi.object({
-                    number: Joi.number().required(),
-                    name: Joi.string().required(),
-                    type: Joi.string().required(),
-                    autofill: Joi.object({
-                      type: Joi.string().required(),
-                      default: Joi.alternatives().try(
-                        Joi.array().items(Joi.string()),
-                        Joi.string().allow('', null)
-                      ),
-                      cases: Joi.array().items(
-                        Joi.object({
-                          queries: Joi.array().items(
-                            Joi.object({
-                              field: Joi.string(),
-                              operator: Joi.string(),
-                              criteria: Joi.alternatives().try(
-                                Joi.string(),
-                                Joi.number(),
-                                Joi.boolean()
-                              ).required(),
-                              clauses: Joi.array().items(
-                                Joi.object({
-                                  field: Joi.string().required(),
-                                  operator: Joi.string().required(),
-                                  criteria: Joi.alternatives().try(
-                                    Joi.string(),
-                                    Joi.number(),
-                                    Joi.boolean()
-                                  ).required()
-                                })
-                              )
-                            })
-                            .when(Joi.object({ operator: Joi.any().valid('$or') }), {
-                              then: Joi.object({
-                                clauses: Joi.required(),
-                                field: Joi.forbidden(),
-                                criteria: Joi.forbidden()
-                              }),
-                              otherwise: Joi.object({
-                                clauses: Joi.forbidden(),
-                                field: Joi.required(),
-                                criteria: Joi.required()
-                              })
-                            })
-                          ),
-                          value: Joi.string().required()
-                        })
-                      ).required()
-                    }).allow('', null),
-                    options: Joi.array().items(Joi.object({
-                      label: Joi.string().required(),
-                      value: Joi.string().required()
-                    })).when('type', {
-                      is: 'RADIO',
-                      then: Joi.required()
-                    }),
-                    validationType: Joi.string(),
-                    validations: Joi.array().items(Joi.object({
-                      type: Joi.string().required(),
-                      params: Joi.array().required()
-                    })),
-                    value: Joi.alternatives().try(
-                      Joi.array().items(Joi.string()),
-                      Joi.string().allow('', null)
-                    ),
-                    evidence: Joi.string().allow(null)
-                  }).and('validationType', 'validations')
-                ),
-                allIds: Joi.array().items(Joi.string()).required()
-              }),
-              evidences: Joi.array().items(Joi.string()).required()
-            })
-          ),
-          allIds: Joi.array().items(Joi.string()).required()
-        })
-      });
-
+      const schema = formSchema;
       const validatedForm = await Joi.validate(inputs.form, schema, {
         stripUnknown: true
       });
